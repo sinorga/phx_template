1098d2c278895e3f588204e91ed224fc4939f372
Orga Shih <orga.shih@gmail.com>
2019-01-24 20:51:34 +0800

    show the current app version on page

--- lib/phx_template_web/controllers/page_controller.ex
+++ lib/phx_template_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule PhxTemplateWeb.PageController do
   use PhxTemplateWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    {:ok, version} = :application.get_key(:phx_template, :vsn)
+    render(conn, "index.html", version: version)
   end
 end
